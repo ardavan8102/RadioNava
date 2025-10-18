05872296de807a7d0b86f0fa60ca4d68be4ccada
Ardavan Eskandari <eskandariardavan81@gmail.com>
2025-10-18 19:54:51 +0330

    splash screen added

---
+++ lib/splash.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  double _opacity = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Appear ANimation
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() => _opacity = 1);
+    });
+
+    // Navigate to HOME
+    Timer(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacementNamed('/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _opacity,
+          duration: Duration(seconds: 3),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/img/logo.png', width: 200, height: 200),
+
+              SizedBox(height: 20),
+
+              Text(
+                'Radio Nava',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+              ),
+
+              Text(
+                'Catch the Melody',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.red.shade900,
+                  letterSpacing: 4,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
